f41d33b37b691be7e71fcd4484ed7dc5b12d4a46
jangseop-sec <kgh010529@gmail.com>
2025-07-06 16:26:58 +1000

    fix(build.sh) incremental build system

--- build.sh
+++ build.sh
@@ -3,61 +3,120 @@
 INCLUDE_PATH="include"
 BUILD_PATH="build"
 
-# clean build folder
-mkdir -p $BUILD_PATH && cd $BUILD_PATH && rm -rf *
 
+# count file
+file_list=$(find . -type f \( -name "*.c" -o -name "*.cpp" \))
+count=$(echo "$file_list" | wc -l)
+idx=1
+
+# clean build folder
+mkdir -p $BUILD_PATH && cd $BUILD_PATH 
+
+
+msg_pkgs=(
+  action_msgs/msg/detail
+  action_msgs/srv/detail
+  builtin_interfaces/msg/detail
+  lifecycle_msgs/msg/detail
+  lifecycle_msgs/srv/detail
+  rcl_interfaces/msg/detail
+  rcl_interfaces/srv/detail
+  rosgraph_msgs/msg/detail
+  statistics_msgs/msg/detail
+  std_msgs/msg/detail
+  unique_identifier_msgs/msg/detail
+)
 
 c_pkgs=(
-  rcl
   rcl/rcl
   rcl/rcl_lifecycle
   rcl/rcl_yaml_param_parser
   rcl_action
-  rcl_logging_interfaces
+  rcl_logging_interface
   rcutils
   rmw
-  rosidl
   rosidl/rosidl_runtime_c
+  rosidl/rosidl_typesupport_introspection_c
   tracetools
 )
 
 cpp_pkgs=(
   ament_index_cpp
-  rclcpp
+  rcpputils
+  rcl_logging_interface
   rclcpp/rclcpp
   rclcpp/rclcpp/contexts
   rclcpp/rclcpp/detail
   rclcpp/rclcpp/exceptions
   rclcpp/rclcpp/executors
   rclcpp/rclcpp/node_interfaces
-  rclcpp/rclcpp/wait_set_policies
-  rclcpp/rclcpp_actions
+  rclcpp/rclcpp/wait_set_policies/detail
+  rclcpp/rclcpp_action
   rclcpp/rclcpp_lifecycle
   rclcpp/rclcpp_lifecycle/node_interfaces
-  rcpputils
+  libstatistics_collector/collector
+  libstatistics_collector/moving_average_statistics
+  rosidl/rosidl_typesupport_introspection_cpp
+  tracetools
 )
 
 # c based packages compilation
-echo "compile C based packages"
+echo "[+] compile C based packages"
 for pkg in "${c_pkgs[@]}"; do
-  echo "compile $pkg package..."
   for src in ../src/$pkg/*.c; do
     filename="${src##*/}"
     obj="${pkg//\//__}__${filename%.c}.o"
-    echo "compile $src => $obj ..."
-    gcc -c -I ../$INCLUDE_PATH $src -o $obj
+    if [ ! -f "$obj" ] || [ "$src" -nt "$obj" ]; then
+      echo "[$idx/$count] compile $src => $obj ..."
+      gcc -c -I ../$INCLUDE_PATH $src -o $obj
+    else
+      echo "[$idx/$count] $filename is up-do-date"
+    fi
+    idx=$((idx+1))
   done
 done
 
 # cpp based packages compilation
-echo "compile C++ based pakcages"
+echo "[+] compile C++ based pakcages"
 for pkg in "${cpp_pkgs[@]}"; do
-  echo "compile $pkg package..."
   for src in ../src/$pkg/*.cpp; do
     filename="${src##*/}"
     obj="${pkg//\//__}__${filename%.cpp}.o"
-    echo "compile $src => $obj ..."
-    g++ -c -I ../$INCLUDE_PATH $src -o $obj
+    if [ ! -f "$obj" ] || [ "$src" -nt "$obj" ]; then
+      echo "[$idx/$count] compile $src => $obj ..."
+      g++ -c -I ../$INCLUDE_PATH $src -o $obj
+    else
+      echo "[$idx/$count] $filename is up-do-date"
+    fi
+    idx=$((idx+1))
+  done
+done
+
+# msg packages compilation
+echo "[+] compile message/service based packages"
+for pkg in "${msg_pkgs[@]}"; do
+  for src in ../include/$pkg/*.cpp; do
+    filename="${src##*/}"
+    obj="${pkg//\//__}__${filename%.cpp}_cpp.o"
+
+    if [ ! -f "$obj" ] || [ "$src" -nt "$obj" ]; then
+      echo "[$idx/$count] compile $src => $obj ..."
+      g++ -c -I ../$INCLUDE_PATH $src -o $obj
+    else
+      echo "[$idx/$count] $filename is up-do-date"
+    fi
+    idx=$((idx+1))
+  done
+  for src in ../include/$pkg/*.c; do
+    filename="${src##*/}"
+    obj="${pkg//\//__}__${filename%.c}_c.o"
+    if [ ! -f "$obj" ] || [ "$src" -nt "$obj" ]; then
+      echo "[$idx/$count] compile $src => $obj ..."
+      gcc -c -I ../$INCLUDE_PATH $src -o $obj
+    else
+      echo "[$idx/$count] $filename is up-do-date"
+    fi
+    idx=$((idx+1))
   done
 done
 
