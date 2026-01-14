5f836b00807e7899c446cd9fd091609ac0e8f4ef
jangseop-sec <kgh010529@gmail.com>
2026-01-14 23:41:06 +1000

    fix remove obslete macro

--- include/rclcpp/node_impl.hpp
+++ include/rclcpp/node_impl.hpp
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#pragma message(">>> USING EDITED node_impl.hpp <<<")
 
 #ifndef RCLCPP__NODE_IMPL_HPP_
 #define RCLCPP__NODE_IMPL_HPP_
